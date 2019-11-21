7f29ae64354547f704b084de5eda1de7dbb5fe97
rovercarlosb <carlosb20052009@gmail.com>
2019-11-21 12:41:56 -0400

    cambios en las migraciones

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration {
 		Schema::create('users', function (Blueprint $table) {
 			$table->bigIncrements('id');
 			$table->string('name');
-			$table->string('email')->unique();
+			$table->string('email');
 			$table->timestamp('email_verified_at')->nullable();
 			$table->string('password');
 			$table->enum('rol', ['admin', 'employee'])->default('employee');
